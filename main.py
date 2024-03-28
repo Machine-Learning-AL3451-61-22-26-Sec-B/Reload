0e04869c1f4c28e9a953fc9ccbcb8eb7ddd8bb49
prakash22aib22 <126764661+prakash22aib22@users.noreply.github.com>
2024-03-28 12:01:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from io import StringIO
 
 def learn(concepts, target):
     '''
@@ -10,14 +9,13 @@ def learn(concepts, target):
         concepts - a data frame with all the features
         target - a data frame with corresponding output values
     '''
-    # Initialise S0 with the first instance from concepts
+    # Initialize S0 with the first instance from concepts
     specific_h = concepts[0].copy()
 
     general_h = [["?" for _ in range(len(specific_h))] for _ in range(len(specific_h))]
 
     # The learning iterations
     for i, h in enumerate(concepts):
-
         # Checking if the hypothesis has a positive target
         if target[i] == "Yes":
             for x in range(len(specific_h)):
@@ -46,27 +44,28 @@ def learn(concepts, target):
 def main():
     st.title("Candidate Elimination Algorithm")
 
-    file_path = "ml experiment 1.csv"
+    uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
     
-    try:
-        # Reading CSV data
-        data = pd.read_csv(file_path)
-        st.write("Original Data:")
-        st.write(data)
+    if uploaded_file is not None:
+        try:
+            # Reading uploaded CSV data
+            data = pd.read_csv(uploaded_file)
+            st.write("Original Data:")
+            st.write(data)
 
-        # Separating concept features from Target
-        concepts = np.array(data.iloc[:, :-1])
+            # Separating concept features from Target
+            concepts = np.array(data.iloc[:, :-1])
 
-        # Isolating target into a separate DataFrame
-        target = np.array(data.iloc[:, -1])
+            # Isolating target into a separate DataFrame
+            target = np.array(data.iloc[:, -1])
 
-        s_final, g_final = learn(concepts, target)
-        st.write("\nFinal Specific_h:")
-        st.write(s_final)
-        st.write("\nFinal General_h:")
-        st.write(g_final)
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+            s_final, g_final = learn(concepts, target)
+            st.write("\nFinal Specific_h:")
+            st.write(s_final)
+            st.write("\nFinal General_h:")
+            st.write(g_final)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
